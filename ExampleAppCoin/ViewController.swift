2bc5bb91c42f32c7ff35d0098fb7ef36d9392225
Thales Luquetti Teixeira <thaleslt@ciandt.com>
2020-01-10 22:56:46 -0300

    first commit

---
+++ ExampleAppCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleAppCoin
+//
+//  Created by Thales Luquetti Teixeira on 10/01/20.
+//  Copyright © 2020 Thales Luquetti Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
